61881d8d55846e7d28f547e65edded4af48b75a4
Dinesh Weerapurage <xydinesh@gmail.com>
2016-03-03 16:54:33 -0700

    adding work highlights

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,16 @@
 from fabric.api import local, run
 from fabric.context_managers import cd
 
-def gen_resume(templatename='resume.mako'):
+def gen_resume(templatename='resume.mako', output='resume.json'):
     from mako.template import Template
     from mako.lookup import TemplateLookup
 
     mylookup = TemplateLookup(directories=['.'], module_directory='mako_modules')
     mytemplate = mylookup.get_template(templatename)
-    print(mytemplate.render())
+    with open(output, "w") as f:
+        f.write(mytemplate.render())
+    print "Done!"
+    local('resume test')
 
 def publish():
     local('resume publish')
--- mako_modules/resume.mako.py
+++ mako_modules/resume.mako.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1457025494.823
+_modified_time = 1457045150.877
 _enable_loop = True
 _template_filename = 'resume.mako'
 _template_uri = 'resume.mako'
@@ -18,9 +18,19 @@ def render_body(context,**pageargs):
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         __M_writer = context.writer()
-        __M_writer(u'{\r\n  "basics": { ')
+        __M_writer(u'{\r\n  "basics": {')
         runtime._include_file(context, u'basic.json', _template_uri)
-        __M_writer(u'\r\n  },\r\n  "work": [],\r\n  "volunteer": [],\r\n  "education": [],\r\n  "awards": [],\r\n  "skills": [],\r\n  "publications": [],\r\n  "languages": [],\r\n  "interests": [],\r\n  "references": []\r\n}\r\n')
+        __M_writer(u'},\r\n  "work": [')
+        runtime._include_file(context, u'work.json', _template_uri)
+        __M_writer(u'],\r\n  "volunteer": [],\r\n  "education": [')
+        runtime._include_file(context, u'education.json', _template_uri)
+        __M_writer(u'],\r\n  "awards": [')
+        runtime._include_file(context, u'awards.json', _template_uri)
+        __M_writer(u'],\r\n  "skills": [')
+        runtime._include_file(context, u'skills.json', _template_uri)
+        __M_writer(u'],\r\n  "publications": [],\r\n  "languages": [')
+        runtime._include_file(context, u'laguages.json', _template_uri)
+        __M_writer(u'],\r\n  "interests": [],\r\n  "references": []\r\n}\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -28,6 +38,6 @@ def render_body(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"source_encoding": "ascii", "line_map": {"16": 0, "29": 23, "21": 1, "22": 2, "23": 2}, "uri": "resume.mako", "filename": "resume.mako"}
+{"source_encoding": "ascii", "line_map": {"32": 9, "33": 9, "39": 33, "16": 0, "21": 1, "22": 2, "23": 2, "24": 3, "25": 3, "26": 5, "27": 5, "28": 6, "29": 6, "30": 7, "31": 7}, "uri": "resume.mako", "filename": "resume.mako"}
 __M_END_METADATA
 """
